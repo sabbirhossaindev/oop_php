7f6ad03ffc1bd039092d3af06347afa8a9922974
sabbir hossain <mdsabbir477470@gmail.com>
2023-08-23 05:04:28 +0600

    constructor and function done

--- constructor-config.php
+++ constructor-config.php
@@ -13,7 +13,7 @@ class Constructor{
     }
 
     function get_course_details(){
-        return "Your course name is ". $this->course_name .". Course Fee ".$this->course_fee ."course details " .$this->course_details . " and course duration is ". $this->course_duration .".";
+        return "Your course name is ". $this->course_name .". Course Fee ".$this->course_fee ." course details " .$this->course_details . " and course duration is ". $this->course_duration .".";
     }
 }
 
