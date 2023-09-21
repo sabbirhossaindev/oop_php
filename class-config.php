b2b532b76ff2185074497275a11740bff2b03969
sabbir hossain <mdsabbir477470@gmail.com>
2023-09-21 21:22:00 +0600

    oop project

--- class-config.php
+++ class-config.php
@@ -1,18 +1,18 @@
 <?php
 class Courses{
-    var $course_name; // var is property
-    var $course_fee;
-    var $course_details;
-    var $course_duration;
+    public $course_name; // var is property
+    public $course_fee;
+    public $course_details;
+    public $course_duration;
 
-    function set_course_details($name, $fee, $details, $duration){
+    public function set_course_details($name, $fee, $details, $duration){
         $this->course_name=$name;
         $this->course_fee=$fee;
         $this->course_details=$details;
         $this->course_duration=$duration;
     }
 
-    function get_course_details(){
+    public function get_course_details(){
         return "Your course name is ". $this->course_name .". Course Fee ".$this->course_fee ."course details " .$this->course_details . " and course duration is ". $this->course_duration .".";
     }
 }
