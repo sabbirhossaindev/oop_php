06819deefcbb444082a4e1ec61f9909c09936a3f
sabbir hossain <mdsabbir477470@gmail.com>
2023-09-25 20:25:39 +0600

    oop project const done

--- apply_inhabitance.php
+++ apply_inhabitance.php
@@ -2,6 +2,7 @@
     include("main_inhabitance.php");
     // extends korla ar akta file er sata add kora.
     // inheritance example;
+    
     class OurFamily extends Inheritance{
         public function baba($name){
             return $this->BaBa=$name;
